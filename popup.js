3a0e5c7275eae050a26e2fff9600d1cbb8d5f436
Jacob Sass <80420670+sass-jacob@users.noreply.github.com>
2022-04-23 11:16:21 -0500

    pp

--- popup.js
+++ popup.js
@@ -1,3 +1,4 @@
 
 shopping_urls = ['https://www.amazon.com/s?k=','https://www.ebay.com/sch/i.html?_from=R40&_trksid=p2334524.m570.l1313&_nkw=','https://www.target.com/s?searchTerm=','https://www.walmart.com/search?q=']
 
+research_urls = ['https://scholar.google.com/scholar?hl=en&as_sdt=0%2C44&q='];
