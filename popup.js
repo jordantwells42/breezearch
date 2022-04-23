98480490ac58f03afc4885a4f893c721e1a83192
Jordan Wells <jordantwells@gmail.com>
2022-04-23 10:58:53 -0500

    fixed errors in manifest

--- popup.js
+++ popup.js
@@ -1,3 +1,7 @@
+let shopping = document.getElementById("shopping")
+console.log(shopping)
+
+
 
 shopping_urls = ['https://www.amazon.com/s?k=','https://www.ebay.com/sch/i.html?_from=R40&_trksid=p2334524.m570.l1313&_nkw=','https://www.target.com/s?searchTerm=','https://www.walmart.com/search?q=']
 
